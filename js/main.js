7b01eaa4b8ac599edcbee31215dc7f2802665532
Groundbender <damir24may@mail.ru>
2022-12-02 16:47:35 +0300

    add: modal window open

--- js/main.js
+++ js/main.js
@@ -50,5 +50,54 @@ const swiper = new Swiper('.swiper', {
  
 });
 
+let currentModal; // текущее модальное окно
+let modalDialog;  // белое диалоговое окно 
+let alertModal = document.querySelector("#alert-modal"); // окна с предупреждением
+let feedbackModal = document.querySelector("#feedback-modal"); // окна с предупреждением
 
+const modalButtons = document.querySelectorAll("[data-toggle=modal]"); // переключатели модальных окон 
+modalButtons.forEach((button) => {
+  /* клик по переключателю */
+  button.addEventListener ("click", (event) => {  
+    event.preventDefault();
+    /* определяем текущее открытое окно */      
+    currentModal = document.querySelector(button.dataset.target);
+    /* открываем текущее окно */
+    currentModal.classList.toggle("is-open");
+    currentModal.classList.contains("is-open") ? document.body.style.overflow = "hidden" : document.body.style.overflow = "";
+
+    /* назначаем текущее диалоговое окно */
+    modalDialog = currentModal.querySelector(".modal-dialog");
+    /* отслеживаем клик по окну и пустым областям */
+    currentModal.addEventListener("click", (event) => {
+      /* если клик в пустую область (не диалоговое окно) */
+      if (!event.composedPath().includes(modalDialog)) {
+        /* закрываем окно */
+        currentModal.classList.remove("is-open");
+        currentModal.classList.contains("is-open") ? document.body.style.overflow = "hidden" : document.body.style.overflow = "";
+
+
+      
+        
+      }
+    
+
+
+      
+    });
+
+  }); 
+});
+/* ловим событие нажатие на кнопки */
+document.addEventListener("keyup", (event) => {
+  /* проверяем что это Escape и текущее окно открыто */
+  if (event.key == "Escape" && currentModal.classList.contains("is-open")) {
+    /* закрываем текущее окно */
+    currentModal.classList.toggle("is-open");
+}
+currentModal.classList.contains("is-open") ? document.body.style.overflow = "hidden" : document.body.style.overflow = "";
+
+
+
+});
 
